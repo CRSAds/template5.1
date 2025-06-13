c789d117f8209f186dcdf1385f65a4fc1a743f07
CRSAds <yuri@crsadvertising.com>
2025-06-13 14:06:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 import initFlow from './initFlow.js';
 import setupFormSubmit from './formSubmit.js';
 import { setupImageFix } from './imageFix.js';
+import setupSovendus from './setupSovendus.js';
 
 // Start logica
 setupImageFix();
